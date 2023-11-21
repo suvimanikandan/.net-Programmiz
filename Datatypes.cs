ad3b9b91450c73abc3df9d41758ad23d92be07bf
swetha_suvi <80690771+suvimanikandan@users.noreply.github.com>
2023-11-21 15:40:16 +0530

    Create Datatypes.cs

---
+++ Datatypes.cs
@@ -0,0 +1,13 @@
+using System;
+namespace DataType
+{
+    class ULongExample
+    {
+        public static void Main(string[] args)
+        {
+            ulong range = 17091821871L;
+            Console.WriteLine(range);
+        }
+    }
+}
+W
